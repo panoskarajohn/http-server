994bd83cd53095ba5eeee64dd51683dc253071a5
Panagiotis <panosakarajohn@hotmail.com>
2022-08-21 22:46:05 +0300

    Even fibonacci numbers sum up until 4000000 fib number

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,14 @@ use rand::Rng;
 
 fn main() {
   //guessing_game();
-  let sum = sum_multiples_of_three_and_five(1000);
+  //let sum = sum_multiples_of_three_and_five(1000);
+  let sum = sum_fibonacci_numbers();
   println!("{sum}");
   
 }
 
+///
+/// A small guesing game for you to play
 fn guessing_game() {
     println!("Guess the number");
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -58,5 +61,32 @@ fn sum_multiples_of_three_and_five(n :u32) -> u32
     }
     // we do not need a return statement we just
     // write the value we want to return without the ;
+    sum
+}
+
+
+/// Returns the sum of all even fib numbers up to 4_000_000 fib number
+fn sum_fibonacci_numbers() -> u32
+{
+    let mut a = 0;
+    let mut b = 1;
+    let mut sum = 0;
+
+    loop {
+        let temp = a + b;
+        a = b;
+        b = temp;
+
+        if temp > 4_000_000
+        {
+            break;
+        }
+
+        if b % 2 == 0
+        {
+            sum += b
+        }
+    }
+
     sum
 }
