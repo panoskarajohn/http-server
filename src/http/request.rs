a2e08c63abc649407c1d8d1fce7e8a2bd7744e8c
Panagiotis <panosakarajohn@hotmail.com>
2022-09-17 23:52:23 +0300

    Custom Error

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,5 +1,9 @@
+use std::str;
+use std::str::Utf8Error;
 use super::method::Method;
 use std::convert::TryFrom;
+use std::error::Error;
+use std::fmt::{Result as FmtResult, Display, Formatter, Debug};
 
 pub struct Request {
     path: String, 
@@ -7,19 +11,56 @@ pub struct Request {
     method: Method 
 }
 
-
-impl Request {
-    fn from_byte_array(buf: &[u8]) -> Result<Self, String> {
-
-    }
-}
 // This is how conversions are meant to happen in Rust
 // Implementing try_from trait(interface)
 impl TryFrom<&[u8]> for Request {
-    type Error = String;
+    type Error = ParseError;
 
+    //GET /search?name=abc&sort=1 HTTP/1.1
     fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
-        
+        let request = str::from_utf8(buffer)?;
     }
 
 }
+
+pub enum ParseError {
+
+    InvalidRequest,
+    InvalidEncoding, 
+    InvalidProtocol,
+    InvalidMethod,
+}
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidMethod => "Invalid Method", 
+            Self::InvalidEncoding => "Invalid Encoding", 
+            Self::InvalidProtocol => "Invalid Protocol", 
+            Self::InvalidRequest => "Invalid Request", 
+        }
+    }
+}
+
+impl From<Utf8Error> for ParseError {
+
+    fn from(_: Utf8Error) -> Self {
+       Self::InvalidEncoding 
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult{
+        write!(f, "{}", self.message())
+        
+    }
+}
+
+impl Debug for ParseError {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult{
+        write!(f, "{}", self.message())
+        
+    }
+}
+
+impl Error for ParseError {}
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,8 @@
 use std::net::TcpListener;
 use std::io::Read; // this is a trait similar to an interface
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
 pub struct Server {
     address: String,
@@ -30,6 +33,14 @@ impl Server {
                         Ok(_) => {
 
                             println!("Received a requst: {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse a request: {}", e),
+
+                            }
+                            
+
                         },
                         Err(e) => println!("Failed to read from connection {}", e)
                     } 
