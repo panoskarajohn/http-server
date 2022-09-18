b0da56cb117f2aa85a79c18345422c2af4437f60
Panagiotis <panosakarajohn@hotmail.com>
2022-09-18 16:44:27 +0300

    Fixed directory traversal attack

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -14,7 +14,17 @@ impl WebsiteHandler {
 
     fn read_file(&self, file_path: &str) -> Option<String> {
         let path = format!("{}/{}", self.public_path, file_path);
-        fs::read_to_string(path).ok()
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attact Attempted: {}", file_path);
+                    None
+                }
+            },
+            Err(_) => None,
+        }
     }
 }
 
@@ -26,8 +36,12 @@ impl Handler for WebsiteHandler {
         match request.method() {
             Method::GET => match request.path() {
                 "/" => Response::new(StatusCode::Ok, self.read_file("index.html")),
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello!</h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")),
+                path => match self.read_file(path) {
+
+                    Some(content) => Response::new(StatusCode::Ok, Some(content)),
+                    None => Response::new(StatusCode::NotFound, None),
+                }
             }
             _ => Response::new(StatusCode::NotFound, None)
         }
